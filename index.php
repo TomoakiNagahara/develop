d8530b545ed1e9e454550db5bc209d8bbc94640f
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2016-11-16 13:30:59 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * index.php
+ *
+ * @created   2016-06-09
+ * @version   1.0
+ * @package   localhost
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+include('../core/Bootstrap.php');
+header("Content-type: text/html; charset=utf-8");
+include('index.phtml');
