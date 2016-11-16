549079299588384ae6a6f0b429bb1625de247f8e
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2016-11-16 13:49:44 +0900

    New: mark.php, dump.php

---
+++ dump.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * develop:/dump.php
+ *
+ * @created   2016-11-16
+ * @version   1.0
+ * @package   localhost
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+$arr = array(0, 1, 0.1, -1, "1", true, false, null);
+$ass = array("key"=>"value");
+$obj   = new stdClass();
+$obj->key = 'value';
+$obj->obj = new stdClass();
+$obj->obj->key = 'value';
+
+//	...
+$os['apple'][] = 'iOS';
+$os['apple'][] = 'tvOS';
+$os['apple'][] = 'macOS';
+$os['microsoft'][] = 'MS-DOS';
+$os['microsoft'][] = 'Windows';
+$os['google'][] = 'Android';
+$arr['OS'] = $os;
+
+//	...
+D([1,null]);
+D($arr);
+D($ass);
+D($obj);
---
+++ mark.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * develop:/mark.php
+ *
+ * @created   2016-11-16
+ * @version   1.0
+ * @package   localhost
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+$arr = [1, 2];
+$arr[] = '3';
+$arr['bool'] = true;
+$arr['null'] = null;
+
+//	...
+$obj = new stdClass();
+$obj->foo = 1;
+
+//	...
+D();
+D(null);
+D(true);
+D(false);
+D(1);
+D(1.0);
+D(1.1);
+D(-1);
+D(-1.0);
+D(-1.1);
+D("1");
+D("日本語");
+D($obj);
+D($arr);
