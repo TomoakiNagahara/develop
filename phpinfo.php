5396e3fa9fc9b193d9e018bae1270093e1d54972
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2016-11-29 18:44:48 +0900

    New: phpinfo.php

---
+++ phpinfo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * develop:/phpinfo.php
+ *
+ * @created   2016-11-29
+ * @version   1.0
+ * @package   develop
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+phpinfo();
