0e0f5ab30b5a6d027492bd8dc2aaff0c7e5e187f
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2016-12-01 02:25:49 +0900

    Fix: sql.php - update, delete, count

--- sql.php
+++ sql.php
@@ -33,8 +33,13 @@ if(!$sql = Unit::Factory('sql') ){
 //	database
 $sql->SetDatabase($db);
 
+//	...
+$id		 = ifset($_GET['id']);
+$text	 = ifset($_GET["text"]);
+$delete	 = ifset($_GET["delete"]);
+
 //	insert
-if( $text = ifset($_GET["text"]) ){
+if( $id === null and $text ){
 	$args = [];
 	$args['table'] = 't_test';
 	$args['set']['text'] = $text;
@@ -45,15 +50,50 @@ if( $text = ifset($_GET["text"]) ){
 	}
 }
 
+//	update
+if( $id and $text ){
+	$args = [];
+	$args['table'] = 't_test';
+	$args['limit'] = 1;
+	$args['set']['text'] = $text;
+	$args['where']['id']['value'] = $id;
+	if( $qu = $sql->Update($args) ){
+		$io = $db->Query($qu);
+		d($qu);
+		d($io);
+	}
+}
+
+//	delete
+if( $id and $delete ){
+	$args = [];
+	$args['table'] = 't_test';
+	$args['limit'] = 1;
+	$args['where']['id']['value'] = $id;
+	if( $qu = $sql->Delete($args) ){
+		$io = $db->Query($qu);
+		d($qu);
+		d($io);
+	}
+}
+
 //	select
 $args = [];
 $args['table'] = 't_test';
 $args['limit'] = 10;
-//$args['column']	 = 'id, text';
-$args['where']['id']['eva']   = '>=';
-$args['where']['id']['value'] = '1';
+$args['offset'] = 0;
+$args['column'] = ifset($_GET['column']);
+$args['where']['id']['evalu'] = '>=';
+$args['where']['id']['value'] = $id;
 if( $qu = $sql->Select($args) ){
 	$records = $db->Query($qu);
+	d($qu);
+	d($records);
+}
+
+//	count
+if( $qu = $sql->Count($args) ){
+	$records = $db->Query($qu, 'count');
 	d($qu);
 	d($records);
 }
