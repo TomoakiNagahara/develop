165f05f1200bfae33496a96e2f446607b333d0f3
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2016-12-07 15:44:01 +0900

    Chg: path.php - Look and feel.

--- path.php
+++ path.php
@@ -8,6 +8,7 @@
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
-d(CompressPath(__FILE__));
-d(ConvertPath('app:/index.php'));
-d(ConvertURL('app:/foo/bar/index.php'));
+?>
+CompressPath：__FILE__ --> <?= CompressPath(__FILE__) ?><br/>
+ConvertPath：app:/index.php --> <?= ConvertPath('app:/index.php') ?><br/>
+ConvertURL：app:/foo/bar/index.php --> <?= ConvertURL('app:/foo/bar/index.php') ?><br/>
