b12cfe3bb8d1d94e030d95315c0e4f313fe0dfc0
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2017-01-26 14:04:04 +0900

    New: database.php - QQL is Quick select Query Language.

--- database.php
+++ database.php
@@ -20,3 +20,9 @@ $args['user']		 = 'test';
 $args['password']	 = '';
 $args['charset']	 = 'utf8';
 d($db->Connect($args));
+
+//	Quick select.
+d($db->Quick(" test.t_test.id = 12 "));
+d($db->Quick(" test.t_test.id > 12 "));
+d($db->Quick(" test.t_test.id < 13 "));
+d($db->Quick(" text <- test.t_test "));
