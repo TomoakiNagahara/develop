f8942637155de8d9c6184f78b1c5d0f503f3d084
Tomoaki -imac21r <tomoaki.nagahara@gmail.com>
2017-03-15 16:27:45 +0900

    FIX: openssl.php

--- openssl.php
+++ openssl.php
@@ -9,7 +9,10 @@
  * @copyright Tomoaki Nagahara All right reserved.
  */
 //	...
-d( function_exists('openssl_encrypt') );
+if(!function_exists('openssl_encrypt')){
+	Html::E("Does not enable openssl.");
+	return;
+}
 
 //	...
 $iv       = "1234567890123456";
@@ -18,12 +21,24 @@ $method   = 'aes-256-cbc';
 $option   = 0;
 
 //	...
-$str = "test";
-$enc = openssl_encrypt($str, $method, $password, $option, $iv);
-$dec = openssl_decrypt($enc, $method, $password, $option, $iv);
-d($str, $enc, $dec);
+$request = Http::Request();
+if( $str = ifset($request['text']) ){
+	$enc = openssl_encrypt($str, $method, $password, $option, $iv);
+	$dec = openssl_decrypt($enc, $method, $password, $option, $iv);
 
-//	...
+	//	...
+	$arg = [];
+	$arg['io']  = $str === $dec ? true: false;
+	$arg['str'] = $str;
+	$arg['enc'] = $enc;
+	$arg['dec'] = $dec;
+	d($arg);
+}
+
+/** Use to command line decrypt.
+ *
+ * @see http://qiita.com/zaburo/items/ffb7728cb9a9efe907d9
+ */
 function strtohex($x)
 {
 	$s = '';
@@ -32,3 +47,8 @@ function strtohex($x)
 	}
 	return $s;
 }
+?>
+<form method="post" action="./?file=openssl">
+	<textarea name="text" style="width: 90vw; height: 50vh;"><?= $str ?></textarea><br/>
+	<button> Submit </button>
+</form>
