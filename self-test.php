1cb1bf9f721972cf5fa8766329fc25a3f23e909b
Tomoaki -imac21r <tomoaki.nagahara@gmail.com>
2017-04-12 13:15:50 +0900

    FIX: self-test

--- self-test.php
+++ self-test.php
@@ -15,14 +15,14 @@ if(!Unit::Load('self-test')){
 }
 
 //	...
-$json   = file_get_contents('self-test.json');
-$config = json_decode($json, true);
-//d('config', $config);
+$json = file_get_contents('config.json');
+$conf = json_decode($json, true);
+//d('conf', $conf);
 
 //	...
-$result = Inspector::Inspect($config);
+$result = Inspector::Inspections($conf);
 //d('inspection result', $result);
 ?>
-<div>
-
-</div>
+<div class="inspection"><?php echo json_encode($result) ?></div>
+<script src="self-test.api.php"></script>
+<?php d($result) ?>
